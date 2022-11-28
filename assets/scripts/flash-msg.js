323efce28e57347b68d7d2a7c489183ee0027046
Peter Johnson <5164283+delphidabbler@users.noreply.github.com>
2022-11-28 10:17:43 +0000

    Rewrite cookies.js (#54)
    * Rewrite cookies.js
    
    Now this script is the same as that on the main DelphiDabbler website.
    
    * Update flash-msg.js
    
    Modify re changes to cookies.js
    Also fix code that detects when no flash message present.
    Limit cookie expiry to 1yr re EU regs.
    
    * Regenerate Gemfile.lock

--- assets/scripts/flash-msg.js
+++ assets/scripts/flash-msg.js
@@ -1,22 +1,27 @@
 /**
  * Enables "flash" messages to persist their display state. Once user has
  * the close button a cookie is set that ensures the flash is not displayed
- * again.
+ * again for a year.
+ *
+ * (C) 2022, Peter Johnson | MIT License | Part of github.com/ddablib/ddablib.github.io
  */
 
 $( function() {
-    var flash = $("#flash-popup");
-    var closeBtn = $("#flash-popup-close");
-    if ( ! flash ) {
+    const flash = $("#flash-popup");
+    if ( flash.length === 0 ) {
+        // there is no flash message in document: nothing to do
         return;
     }
+    var closeBtn = $("#flash-popup-close");
+   
     var id = flash.data('flash-id');
     var cookieName = 'dd-ddablib--flash-' + id;
     closeBtn.on('click', function () {
-        Cookie.write(cookieName, "dismissed", 3650);
+        // set cookie for 1yr: can't set for more per EU regs
+        Cookie.set(cookieName, "dismissed", 365);
         flash.hide();
     });
-    if ( ! Cookie.read(cookieName) ) {
+    if ( ! Cookie.isSet(cookieName) ) {
         flash.show();
     }
     else {
